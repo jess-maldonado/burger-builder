dc7e5c2fed2cbe3a0589cd4ff226fe07b73737a9
jessmal11 <jess.maldonado1@gmail.com>
2020-04-20 09:27:20 -0400

    added order button & price update

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -13,14 +13,13 @@ const Burger = (props) => {
         } )
         .reduce((arr, el) => {
             return arr.concat(el)
-        }, [])
+        }, []);
 
         if (transformedIngredients.length === 0) {
             transformedIngredients = <p>Please start adding ingredients!</p>
         }
 
-        console.log(transformedIngredients)
-
+        
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
